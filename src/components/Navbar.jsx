d843d9d1eee20eeba21c6a97a8e6e7a09d2e9b6f
Raiyan Shamim <razash139@gmail.com>
2023-04-06 10:27:55 +0300

    match figma navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import { HashLink as Link } from "react-router-hash-link";
 import { useTranslation } from "react-i18next";
-import React, {useRef} from "react"
+import React, { useRef } from "react";
 
-import englishFlag from '../assets/english-flag.svg'
-import finnishFlag from '../assets/finnish-flag.svg'
+import englishFlag from "../assets/english-flag.svg";
+import finnishFlag from "../assets/finnish-flag.svg";
 
 const Navbar = ({ open, setOpen }) => {
   const { t, i18n } = useTranslation();
@@ -12,47 +12,108 @@ const Navbar = ({ open, setOpen }) => {
   const MenuRef = useRef();
   const MobileNavRef = useRef();
 
-  const openMobileMenu = () =>{
+  const openMobileMenu = () => {
     MenuRef.current.style.display = "none";
     openMenuRef.current.style.display = "block";
     MobileNavRef.current.style.display = "block";
-  }
+  };
 
-  const closeMobileMenu = () =>{
+  const closeMobileMenu = () => {
     MenuRef.current.style.display = "block";
     openMenuRef.current.style.display = "none";
     MobileNavRef.current.style.display = "none";
-  }
-
+  };
 
   return (
     <>
       <div className="sticky top-0 z-50">
         <nav className="bg-zinc-950 py-4 border-b-2 border-red-600">
           <div className="container flex gap-4 justify-between">
-            <Link to="/" className="flex">
-                <img className="mr-4" src="/logo.svg" alt="" />
-                <span className="text-xl font-heading">{t("markku-customs")}</span>
+            <Link to="/" className="flex items-center gap-4">
+              <img
+                className="h-8 w-8"
+                src="/logo.svg"
+                alt=""
+                height={32}
+                width={32}
+              />
+              <span className="text-xl font-heading">
+                {t("markku-customs")}
+              </span>
             </Link>
             <div className="hidden sm:flex gap-6">
-              <ul className="nav-bar-items grow flex gap-8 justify-between items-center -my-4">
-                <li className="text-xs uppercase tracking-[.30em] font-semibold hover:text-red-600">
-                  <Link to="/#home">
+              <ul className="flex -my-4">
+                <li className="text-xs uppercase tracking-[.30em] font-semibold h-full px-6 flex items-center hover:bg-zinc-900 pointer-events-none transition duration-200">
+                  <Link to="/#home" className="pointer-events-auto">
                     {t("home")}
                   </Link>
                 </li>
-                <li className="text-xs uppercase tracking-[.30em] font-semibold hover:text-red-600">
-                  <Link to="/#store">
+                <li className="text-xs uppercase tracking-[.30em] font-semibold h-full px-6 flex items-center hover:bg-zinc-900 pointer-events-none transition duration-200">
+                  <Link to="/#store" className="pointer-events-auto">
                     {t("store")}
                   </Link>
                 </li>
-                <li className="text-xs uppercase tracking-[.30em] font-semibold hover:text-red-600">
-                  <Link to="/#contact">
+                <li className="text-xs uppercase tracking-[.30em] font-semibold h-full px-6 flex items-center hover:bg-zinc-900 pointer-events-none transition duration-200">
+                  <Link to="/#contact" className="pointer-events-auto">
                     {t("contact")}
                   </Link>
                 </li>
               </ul>
-              <div className="nav-bar-items grow flex gap-4 items-center">
+              <div className="flex gap-4 items-center">
+                <img
+                  onClick={() => i18n.changeLanguage("en")}
+                  className="hover:scale-110 h-4 transition duration-100"
+                  src={englishFlag}
+                  alt="English"
+                />
+                <img
+                  onClick={() => i18n.changeLanguage("fi")}
+                  className="hover:scale-110 h-4 transition duration-100"
+                  src={finnishFlag}
+                  alt="Suomi"
+                />
+              </div>
+            </div>
+            <div className="block sm:hidden">
+              <img
+                ref={MenuRef}
+                onClick={openMobileMenu}
+                className="closed-menu"
+                src="/menu.png"
+                alt=""
+              />
+              <img
+                ref={openMenuRef}
+                onClick={closeMobileMenu}
+                className="open-menu"
+                src="/menu-open.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </nav>
+        <nav
+          ref={MobileNavRef}
+          className="mobile-nav border-b-2 bg-zinc-950 border-red-600"
+        >
+          <ul className="flex gap-4 flex-col justify-between items-center p-3">
+            <Link to="/#home">
+              <li className="text-base hover:text-red-600 hover:scale-110">
+                {t("home")}
+              </li>
+            </Link>
+            <Link to="/#store">
+              <li className="text-base hover:text-red-600 hover:scale-110">
+                {t("store")}
+              </li>
+            </Link>
+            <Link to="/#contact">
+              <li className="text-base hover:text-red-600 hover:scale-110">
+                {t("contact")}
+              </li>
+            </Link>
+            <li>
+              <div className="grow flex gap-4 items-center">
                 <img
                   onClick={() => i18n.changeLanguage("en")}
                   className="hover:scale-110 h-4 transition duration-100"
@@ -66,49 +127,10 @@ const Navbar = ({ open, setOpen }) => {
                   alt="Finnish"
                 />
               </div>
-            </div>
-            <div className="menu-container">
-              <img ref={MenuRef} onClick={openMobileMenu} className="closed-menu"  src="/menu.png" alt="" />
-              <img ref={openMenuRef} onClick={closeMobileMenu} className="open-menu" src="/menu-open.png" alt="" />
-            </div>
-          </div>
+            </li>
+          </ul>
         </nav>
-        <nav ref={MobileNavRef} className="mobile-nav border-b-2 bg-zinc-950 border-red-600">
-            <ul className="flex gap-4 flex-col justify-between items-center p-3">
-              <Link to="/#home">
-                <li className="text-base hover:text-red-600 hover:scale-110">
-                  {t("home")}
-                </li>
-              </Link>
-              <Link to="/#store">
-                <li className="text-base hover:text-red-600 hover:scale-110">
-                  {t("store")}
-                </li>
-              </Link>
-              <Link to="/#contact">
-                <li className="text-base hover:text-red-600 hover:scale-110">
-                  {t("contact")}
-                </li>
-              </Link>
-                  <li>
-                      <div className="grow flex gap-4 items-center">
-                          <img
-                          onClick={() => i18n.changeLanguage("en")}
-                          className="hover:scale-110 h-4 transition duration-100"
-                          src={englishFlag}
-                          alt="English"
-                          />
-                          <img
-                          onClick={() => i18n.changeLanguage("fi")}
-                          className="hover:scale-110 h-4 transition duration-100"
-                          src={finnishFlag}
-                          alt="Finnish"
-                          />
-                      </div>
-                  </li>
-            </ul>
-          </nav>
-        </div>
+      </div>
     </>
   );
 };
