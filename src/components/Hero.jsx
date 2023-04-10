16d4ebc745bf85d9af90476c1e0c28dd72c9ed26
Rixen <Rixenlama@gmail.com>
2023-04-10 10:36:22 +0300

    Responsive hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,17 +7,17 @@ const Hero = () => {
   return (
     <section className="hero" id="home">
       <div className="container">
-        <h1 className="font-heading text-6xl md:text-8xl mt-12 sm:mt-20 hero-text">
+        <h1 className="font-heading text-6xl md:text-8xl mt-12 sm:mt-20 hero-text hero-catchphrase">
           <Trans i18nKey={"hero.heading"}>
             You Build.
             <br />
             We Play.
           </Trans>
         </h1>
-        <p className="mt-6 sm:mt-8 max-w-prose text-sm sm:text-base leading-8 text-zinc-300">
+        <p className="mt-6 sm:mt-8 max-w-prose text-sm sm:text-base leading-8 text-center text-zinc-300">
           {t("hero.description")}
         </p>
-        <div className="flex flex-wrap gap-4 mt-6 sm:mt-8">
+        <div className="flex flex-wrap gap-4 mt-6 sm:mt-8 hero-button-responsive">
           <Link
             to="/#store"
             className="button | bg-red-600 hover:brightness-125"
