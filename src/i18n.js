5caa8886e6ac0933f648ac353fd1d2d402bf3b75
Raiyan Shamim <razash139@gmail.com>
2023-04-10 17:27:10 +0300

    seperate translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,97 +1,16 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
-  'en-US': {
-    translation: {
-      'markku-customs': 'Markku Customs',
-      links: {
-        home: 'Home',
-        store: 'Store',
-        contact: 'Contact',
-      },
-      hero: {
-        heading: 'We Build. <br/> You Play.',
-        description:
-          'We build custom high-performance PCs tailored to your needs and budget, using only the highest quality components. Contact us to get started on your perfect PC!',
-        buttons: {
-          store: 'View Store',
-          contact: 'Contact Us',
-        },
-      },
-      contact: {
-        phone: 'Phone Number',
-        email: 'Email',
-        instagram: 'Instagram',
-        name: 'Name',
-        message: 'Message',
-        'type-message': 'Type your message here...',
-        send: 'Send Message',
-      },
-      variable: 'Variable',
-      order: 'Order Now',
-      'fps-performance': 'FPS Performance',
-      footer: {
-        copyright: '© {{year}} Markku Customs. All rights reserved.',
-        privacy: 'Privacy Policy',
-        terms: 'Terms of Service',
-      },
-    },
-  },
-  'fi-FI': {
-    translation: {
-      'markku-customs': 'Markkulan Kustomsit',
-      links: {
-        home: 'Koti',
-        store: 'Kauppa',
-        contact: 'Yhteystiedot',
-      },
-      hero: {
-        heading: 'We Build. <br/> You Play.',
-        description:
-          'Rakennamme asiakkaan tarpeisiin ja budjettiin räätälöityjä, korkean suorituskyvyn tietokoneita käyttäen vain laadukkaimpia komponentteja. Ota yhteyttä aloittaaksesi täydellisen tietokoneen rakentamisen!',
-        buttons: {
-          store: 'Näytä Valikoima',
-          contact: 'Ota Yhteyttä',
-        },
-      },
-      contact: {
-        phone: 'Puhelinnumero',
-        email: 'Sähköposti',
-        instagram: 'Instagram',
-        name: 'Nimi',
-        message: 'Viesti',
-        'type-message': 'Kirjoita viestisi tähän...',
-        send: 'Lähetä Viesti',
-      },
-      variable: 'Muuttuva',
-      order: 'Tilaa Nyt',
-      'fps-performance': 'FPS-toimivuus',
-      footer: {
-        copyright: '© {{year}} Markku Customs. Kaikki oikeudet pidätetään.',
-        privacy: 'Tietosuojaseloste',
-        terms: 'Käyttöehdot',
-      },
-    },
-  },
-};
+import HttpBackend from 'i18next-http-backend';
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(initReactI18next)
   .use(LanguageDetector)
+  .use(HttpBackend)
   .init({
-    resources,
-    fallbackLng: 'en-US', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
+    fallbackLng: 'en-US',
     interpolation: {
-      escapeValue: false, // react already safes from xss
+      escapeValue: false,
     },
   });
 
