f225f589732e531eafc05e8a9e2a1cf7ee3ba159
Raiyan Shamim <razash139@gmail.com>
2023-04-10 21:03:15 +0300

    translate 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,13 @@
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 import Layout from '../components/Layout';
 import Button from '../components/Button';
 
 const NotFoundPage = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <Helmet>
@@ -15,9 +18,9 @@ const NotFoundPage = () => {
       <Layout>
         <div className="container h-full flex flex-col justify-center items-center">
           <h1 className="font-heading text-8xl">404</h1>
-          <p className="font-semibold">Page Not Found</p>
+          <p className="font-semibold">{t('404.subtitle')}</p>
           <Button as={Link} to="/" className="mt-8">
-            Return Home
+            {t('404.button')}
           </Button>
         </div>
       </Layout>
