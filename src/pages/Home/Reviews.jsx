58463967b402d1b7c1ef1c1638cac1710ee98853
Raiyan Shamim <razash139@gmail.com>
2023-07-26 11:50:20 +0300

    add button

--- src/pages/Home/Reviews.jsx
+++ src/pages/Home/Reviews.jsx
@@ -20,6 +20,14 @@ const Reviews = ({ reviews }) => {
     speed: 500,
     autoplaySpeed: 10000,
     pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -35,15 +43,41 @@ const Reviews = ({ reviews }) => {
             })}
           </Slider>
         </div>
-        <Button
-          as="a"
-          className="w-max"
-          href="https://g.page/r/CUxzjc1LVcAjEB0/review"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {t('write-review')}
-        </Button>
+        <div className="flex flex-wrap gap-4">
+          <Button
+            as="a"
+            className="w-max flex gap-2 item-center"
+            href="https://g.page/r/CUxzjc1LVcAjEB0/review"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {t('write-review')}
+            <img
+              src="/arrow-outward-white.svg"
+              alt=""
+              aria-hidden="true"
+              height={16}
+              width={16}
+            />
+          </Button>
+          <Button
+            as="a"
+            variant="secondary"
+            className="w-max flex gap-2 items-center"
+            href="https://www.google.com/maps/place/Markku+Customs/@60.4319644,21.8945733,10z/data=!4m18!1m9!3m8!1s0x468c7719ea889cb5:0x23c0554bcd8d734c!2sMarkku+Customs!8m2!3d60.4320325!4d22.2242091!9m1!1b1!16s%2Fg%2F11tgf7cv01!3m7!1s0x468c7719ea889cb5:0x23c0554bcd8d734c!8m2!3d60.4320325!4d22.2242091!9m1!1b1!16s%2Fg%2F11tgf7cv01?authuser=2&entry=ttu"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {t('view-reviews')}
+            <img
+              src="/arrow-outward-white.svg"
+              alt=""
+              aria-hidden="true"
+              height={16}
+              width={16}
+            />
+          </Button>
+        </div>
       </div>
     </section>
   );
