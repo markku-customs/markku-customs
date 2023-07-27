c18d0fe7bd047541098451be0ab4737714dd22d6
Raiyan Shamim <razash139@gmail.com>
2023-07-27 17:59:06 +0300

    extract link data

--- src/pages/Home/ContactLinks.jsx
+++ src/pages/Home/ContactLinks.jsx
@@ -2,30 +2,45 @@ import { useTranslation } from 'react-i18next';
 
 import ContactLink from './ContactLink';
 
+const links = [
+  {
+    icon: '/phone-icon.svg',
+    key: 'contact.phone',
+    content: '+358 40 932 3040',
+    href: 'tel: +358409323040',
+    blank: false,
+  },
+  {
+    icon: '/email-icon.svg',
+    key: 'contact.email',
+    content: 'markkucustoms@gmail.com',
+    href: 'mailto: markkucustoms@gmail.com',
+    blank: false,
+  },
+  {
+    icon: '/instagram-icon.svg',
+    key: 'contact.instagram',
+    content: '@markku.customs',
+    href: 'https://www.instagram.com/markku.customs/',
+    blank: true,
+  },
+];
+
 const ContactLinks = () => {
   const { t } = useTranslation();
 
   return (
     <ul className="flex flex-wrap gap-8 lg:gap-12">
-      <ContactLink
-        icon="/phone-icon.svg"
-        title={t('contact.phone')}
-        content="+358 40 932 3040"
-        href="tel: +358409323040"
-      />
-      <ContactLink
-        icon="/email-icon.svg"
-        title={t('contact.email')}
-        content="markkucustoms@gmail.com"
-        href="mailto: markkucustoms@gmail.com"
-      />
-      <ContactLink
-        icon="/instagram-icon.svg"
-        title={t('contact.instagram')}
-        content="@markku.customs"
-        href="https://www.instagram.com/markku.customs/"
-        blank
-      />
+      {links.map(({ icon, key, content, href, blank }) => (
+        <ContactLink
+          key={key}
+          icon={icon}
+          title={t(key)}
+          content={content}
+          href={href}
+          blank={blank}
+        />
+      ))}
     </ul>
   );
 };
