9f50a32599efaeebae1349ff5008b65b08b9a96e
Raiyan Shamim <razash139@gmail.com>
2023-08-03 20:50:10 +0300

    fix badge styling

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,7 +2,10 @@ import PropTypes from 'prop-types';
 
 const Badge = ({ children, ...props }) => {
   return (
-    <span className="bg-zinc-800 px-3 py-1.5 text-xs font-semibold" {...props}>
+    <span
+      className="rounded-full bg-zinc-800 px-4 py-1.5 text-xs font-semibold"
+      {...props}
+    >
       {children}
     </span>
   );
