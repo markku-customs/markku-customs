b35ce4a2c543a3e2572f51e95694fd1d648132da
Raiyan Shamim <razash139@gmail.com>
2023-08-05 19:40:28 +0300

    extend seo component

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
-const SEO = ({ title, description }) => {
+const SEO = ({
+  title,
+  description,
+  url = '/',
+  image = '/img/hero/hero-2xl-1536.webp',
+}) => {
   return (
     <Helmet>
       <title>{title}</title>
@@ -8,16 +14,22 @@ const SEO = ({ title, description }) => {
       <meta name="description" content={description} />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://markkucustoms.com/" />
+      <meta property="og:url" content={`https://markkucustoms.com${url}`} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="/hero.png" />
+      <meta property="og:image" content={`https://markkucustoms.com${image}`} />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://markkucustoms.com/" />
+      <meta
+        property="twitter:url"
+        content={`https://markkucustoms.com${url}`}
+      />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content="/hero.png" />
+      <meta
+        property="twitter:image"
+        content={`https://markkucustoms.com${image}`}
+      />
 
       <meta name="geo.position" content="60.451753;22.267052" />
       <meta name="ICBM" content="60.451753, 22.267052" />
@@ -25,6 +37,18 @@ const SEO = ({ title, description }) => {
       <meta name="geo.placename" content="Turku" />
     </Helmet>
   );
+};
+
+SEO.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  url: PropTypes.string,
+  image: PropTypes.string,
+};
+
+SEO.defaultProps = {
+  url: '/',
+  image: '/img/hero/hero-2xl-1536.webp',
 };
 
 export default SEO;
