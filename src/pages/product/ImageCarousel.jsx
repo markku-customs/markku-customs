99d655b709208af0a3052403d1a38d4ed9db1a18
Raiyan Shamim <razash139@gmail.com>
2023-11-24 16:02:18 +0200

    remove carousel if one image

--- src/pages/product/ImageCarousel.jsx
+++ src/pages/product/ImageCarousel.jsx
@@ -7,20 +7,36 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 const ImageCarousel = ({ images, name }) => {
   return (
     <section className="image-carousel-container flex flex-col">
-      <Carousel useKeyboardArrows dynamicHeight infiniteLoop>
-        {images ? (
-          images['en-US'].map((image) => (
-            <img
-              src={`${getImageSrc(image)}?fm=webp&w=840`}
-              alt={name}
-              key={image.fields.file['en-US'].url}
-              loading="lazy"
-            />
-          ))
+      {images ? (
+        images['en-US'].length > 1 ? (
+          <Carousel useKeyboardArrows dynamicHeight infiniteLoop>
+            {images['en-US'].map((image) => (
+              <img
+                src={`${getImageSrc(image)}?fm=webp&w=840`}
+                alt={name}
+                key={image.fields.file['en-US'].url}
+                loading="lazy"
+              />
+            ))}
+          </Carousel>
         ) : (
-          <img src="/product-default.png" alt={name} />
-        )}
-      </Carousel>
+          <div className="grid place-items-center bg-zinc-900">
+            <img
+              src={`${getImageSrc(images['en-US'][0])}?fm=webp&w=840`}
+              alt={name}
+              className="max-h-[24rem] object-cover"
+            />
+          </div>
+        )
+      ) : (
+        <div className="grid place-items-center bg-zinc-900">
+          <img
+            src="/product-default.png"
+            alt={name}
+            className="max-h-[24rem] object-cover"
+          />
+        </div>
+      )}
     </section>
   );
 };
