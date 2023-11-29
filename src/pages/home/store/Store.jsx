1aef056dc7e4032e2a0a074b69859d3ec9b2fa89
Raiyan Shamim <razash139@gmail.com>
2023-11-29 10:47:49 +0200

    remove suspense

--- src/pages/home/store/Store.jsx
+++ src/pages/home/store/Store.jsx
@@ -1,5 +1,3 @@
-import { Suspense } from 'react';
-
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import useSWR from 'swr';
@@ -20,34 +18,38 @@ const Store = () => {
     <section className="py-12 md:py-16" id="store">
       <Container>
         <SectionHeading className="mb-8">{t('links.store')}</SectionHeading>
-        <Suspense fallback={<Loading error={error} />}>
-          {products?.items.filter((p) => p.fields.isFeatured['en-US']).length >
-            0 && (
-            <div className="mb-8 flex flex-col gap-8">
+        {!products ? (
+          <Loading error={error} />
+        ) : (
+          <>
+            {products?.items.filter((p) => p.fields.isFeatured['en-US'])
+              .length > 0 && (
+              <div className="mb-8 flex flex-col gap-8">
+                {products?.items
+                  .filter((p) => p.fields.isFeatured['en-US'])
+                  .map((product) => {
+                    const { id } = product.sys;
+
+                    return <StoreFeaturedItem product={product} key={id} />;
+                  })}
+              </div>
+            )}
+
+            <div className="store-grid">
               {products?.items
-                .filter((p) => p.fields.isFeatured['en-US'])
+                .filter((p) => !p.fields.isFeatured['en-US'])
                 .map((product) => {
                   const { id } = product.sys;
 
-                  return <StoreFeaturedItem product={product} key={id} />;
+                  return (
+                    <Link to={`/products/${id}`} key={id}>
+                      <StoreItem product={product} />
+                    </Link>
+                  );
                 })}
             </div>
-          )}
-
-          <div className="store-grid">
-            {products?.items
-              .filter((p) => !p.fields.isFeatured['en-US'])
-              .map((product) => {
-                const { id } = product.sys;
-
-                return (
-                  <Link to={`/products/${id}`} key={id}>
-                    <StoreItem product={product} />
-                  </Link>
-                );
-              })}
-          </div>
-        </Suspense>
+          </>
+        )}
       </Container>
     </section>
   );
--- src/pages/terms/index.jsx
+++ src/pages/terms/index.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { useTranslation } from 'react-i18next';
@@ -30,18 +30,22 @@ const TermsPage = () => {
       <SEO title={`${t('footer.terms')} | Markku Customs`} />
 
       <Container className="py-8">
-        <Suspense fallback={<Loading error={error} />}>
-          <h1 className="font-heading text-4xl sm:text-6xl">
-            {t('footer.terms')}
-          </h1>
-          <div className="my-4 w-max bg-zinc-900 px-4 py-2 text-sm text-zinc-400">
-            {t('last-updated')}:{' '}
-            {formatDate(new Date(page.sys.updatedAt), {}, lng)}
-          </div>
-          <section>
-            {documentToReactComponents(page.fields.content[lng], options)}
-          </section>
-        </Suspense>
+        {!page ? (
+          <Loading error={error} />
+        ) : (
+          <>
+            <h1 className="font-heading text-4xl sm:text-6xl">
+              {t('footer.terms')}
+            </h1>
+            <div className="my-4 w-max bg-zinc-900 px-4 py-2 text-sm text-zinc-400">
+              {t('last-updated')}:{' '}
+              {formatDate(new Date(page.sys.updatedAt), {}, lng)}
+            </div>
+            <section>
+              {documentToReactComponents(page.fields.content[lng], options)}
+            </section>
+          </>
+        )}
       </Container>
     </>
   );
