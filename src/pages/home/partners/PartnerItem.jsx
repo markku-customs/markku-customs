5ac9cd07fc9022ff46bbd5a56e6a4f2aacc561c9
Raiyan Shamim <razash139@gmail.com>
2024-01-16 12:27:12 +0200

    arrow

--- src/pages/home/partners/PartnerItem.jsx
+++ src/pages/home/partners/PartnerItem.jsx
@@ -2,6 +2,8 @@ import { Trans } from 'react-i18next';
 
 import { getImageSrc } from '@/utils';
 
+import { ArrowOutwardIcon } from '@/icons';
+
 const PartnerItem = ({ partner }) => {
   const { logo, name, since, link } = partner.fields;
 
@@ -17,15 +19,16 @@ const PartnerItem = ({ partner }) => {
       <div className="grid items-center px-16 py-12">
         <img src={`${getImageSrc(logo['en-US'])}?fm=webp&w=200`} alt="" />
       </div>
-      <div className="flex justify-between bg-zinc-800 p-4">
-        <div>
+      <div className="bg-zinc-800 p-4">
+        <div className="flex items-center gap-1">
           <h3 className="font-semibold">{name['en-US']}</h3>
-          <p className="mt-1 text-sm text-zinc-400">
-            <Trans i18nKey="since" year={year}>
-              Since {{ year }}
-            </Trans>
-          </p>
+          <ArrowOutwardIcon className="h-4 w-4" />
         </div>
+        <p className="mt-1 text-sm text-zinc-400">
+          <Trans i18nKey="since" year={year}>
+            Since {{ year }}
+          </Trans>
+        </p>
       </div>
     </a>
   );
